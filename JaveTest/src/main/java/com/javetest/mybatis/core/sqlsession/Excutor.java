242fd1bfafdd4ed808fa64c5d22e10a9530a2f66
ZhouFeng <zhouf1982@gmail.com>
2018-03-23 16:43:45 +0800

    test

--- JaveTest/src/main/java/com/javetest/mybatis/core/sqlsession/Excutor.java
+++
@@ -1,5 +0,0 @@
-package com.javetest.mybatis.core.sqlsession;
-
-public interface Excutor {
-    public <T> T query(String statement, Object parameter);
-}
--- JaveTest/src/main/java/com/javetest/mybatis/mapper/SysUsersMapper.java
+++
@@ -1,7 +0,0 @@
-package com.javetest.mybatis.mapper;
-
-import com.javetest.mybatis.bean.SysUsers;
-
-public interface SysUsersMapper {
-    SysUsers getSysUserByid(Long id);
-}
